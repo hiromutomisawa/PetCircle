4a044d6f292127442c4d02c8f3f801dedba555ec
hiromutomisawa <peachcocoaht202101@gmail.com>
2021-07-30 10:25:37 +0000

    [add]javascript

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,19 +2,19 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post_blog = PostBlog.find(params[:post_blog_id])
-    favorite = current_user.favorites.new(post_blog_id: post_blog.id)
+    @post_blog = PostBlog.find(params[:post_blog_id])
+    favorite = current_user.favorites.new(post_blog_id: @post_blog.id)
     # favorite = Favorite.new
     # favorite.user_id = current_user.id
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    post_blog = PostBlog.find(params[:post_blog_id])
-    favorite = current_user.favorites.find_by(post_blog_id: post_blog.id)
+    @post_blog = PostBlog.find(params[:post_blog_id])
+    favorite = current_user.favorites.find_by(post_blog_id: @post_blog.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
